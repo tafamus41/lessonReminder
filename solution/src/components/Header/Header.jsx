3a0b5e02c71dc8a0e9ff02186a3d064070a33ce4
asimov <seyranmustafa4@gmail.com>
2024-08-24 04:34:00 +0300

    lesson_reminder

---
+++ solution/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.css"
+
+const Header = () => {
+  return (
+    <div className="Header">
+        <h1><img src="https://secure.meetupstatic.com/photos/event/3/1/b/9/600_488352729.jpeg" alt="" width="50px"/>Clarusway</h1>
+    </div>
+  )
+}
+
+export default Header
